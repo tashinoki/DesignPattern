670427bf0c4c86ca1123bb6feacb1f5e88101410
tashinoki <kenya.112163@gmail.com>
2021-09-04 00:00:01 +0900

    add bride

---
+++ Bridge/Sample/StringDisplayImp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge.Sample
+{
+    internal class StringDisplayImp: IDisplayImp
+    {
+        private string _str;
+
+        private int _width;
+
+        public StringDisplayImp(string str, int width)
+        {
+            _str = str;
+            _width = width;
+        }
+
+        public void RawOpen()
+        {
+            Console.WriteLine();
+        }
+
+        public void RawPrint()
+        {
+            Console.WriteLine($"|{_str}|");
+        }
+
+        public void RawClose()
+        {
+            Console.WriteLine();
+        }
+
+        private void PrintLine()
+        {
+            Console.Write("*");
+
+            for(var i = 0; i < _width; i ++)
+            {
+                Console.Write("-");
+            }
+
+            Console.Write("*");
+        }
+    }
+}
